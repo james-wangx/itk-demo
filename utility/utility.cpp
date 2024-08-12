2eda777348241806061febf21c0c67ad4e9244fc
james-wangx <james_wangx@163.com>
2024-08-12 20:07:28 +0800

    Add ITK template

--- utility/utility.cpp
+++ utility/utility.cpp
@@ -1,20 +1,74 @@
-// utility.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
-int main()
+#include <tcinit/tcinit.h>
+#include <tc/emh.h>
+#include <mld/journal/journal.h>
+
+const int WRONG_USAGE = 100001;
+
+static inline void output_useage()
 {
-    std::cout << "Hello World!\n";
+	std::cout << "\nUSEAGE: utility -u=user -p=password -g=group" << std::endl;
+
+	return;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+static inline void output_filename()
+{
+	char* journal_filename;
+	char* system_log_filename;
+	
+	JOURNAL_ask_file_name(&journal_filename);
+	EMH_ask_system_log_filename(&system_log_filename);
+	std::cout << "journal file name: " << journal_filename << std::endl;
+	std::cout << "system log: " << system_log_filename << std::endl;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	MEM_free(journal_filename);
+	MEM_free(system_log_filename);
+}
+
+int ITK_user_main(int argc, char** argv)
+{
+	output_filename();
+
+	char* usr = ITK_ask_cli_argument("-u=");
+	char* upw = ITK_ask_cli_argument("-p=");
+	char* ugp = ITK_ask_cli_argument("-g=");
+	char* help = ITK_ask_cli_argument("-h=");
+
+	if (!usr || !upw || !ugp || help)
+	{
+		output_useage();
+		return WRONG_USAGE;
+	}
+
+	std::cout << "usr = " << usr << std::endl;
+	std::cout << "upw = " << upw << std::endl;
+	std::cout << "ugp = " << ugp << std::endl;
+
+	ITK_initialize_text_services(0);
+	int status = ITK_init_module(usr, upw, ugp);
+
+	if (status != ITK_ok)
+	{
+		char* error;
+		EMH_ask_error_text(status, &error);
+		std::cout << "Error with ITK_init_module: " << error << std::endl;
+		MEM_free(error);
+		return status;
+	}
+	else
+		std::cout << "Login to Teamcenter success as " << usr << std::endl;
+
+	// Need env: TC_JOURNAL=FULL
+	JOURNAL_comment("Preparing to list tool formats\n");
+	TC_write_syslog("Preparing to list tool formats\n");
+	/* Call your functions between here */
+
+	ITK_exit_module(TRUE);
+
+	std::cout << "Press Enter to continue...";
+	std::cin.get();
+
+	return status;
+}
